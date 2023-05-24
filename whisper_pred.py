39b28cf41ae6db4dc7f8c1d09ed7484b23f46ee5
anthony_wss <b09902033@csie.ntu.edu.tw>
2023-05-24 11:30:27 +0800

    rebuild

--- whisper_pred.py
+++ whisper_pred.py
@@ -1,44 +1,35 @@
 import whisper
 import numpy as np
-
 from datasets import load_dataset
-import dataclasses, iso639
 from tqdm import tqdm
 import os
+from lang.utils import get_iso_693_3
+import pickle as pkl
+import torch
 
-OUTPUT_DIR = "./whisper_results"
-
-def get_iso_693_3(lang_id):
-    if lang_id == 'jw':
-        lang_id = 'jv'
-    language = iso639.Language.match(lang_id)
-    return dataclasses.asdict(language)['part3']
+OUTPUT_DIR = "fleurs_whisper_results"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 fleurs_langID = load_dataset("google/fleurs", "all", split="test") # to download all data
 
-model = whisper.load_model("base")
-preds = []
-true_label = []
+model = whisper.load_model("medium", device='cuda')
+pred = []
+true = []
 
-decode = {i: fleurs_langID.features["lang_id"].names[i].split('_')[0] for i in range(102)}
+decode = {i: get_iso_693_3(fleurs_langID.features["lang_id"].names[i].split('_')[0]) for i in range(102)}
 
-for i in tqdm(range(fleurs_langID.num_rows)):
-    audio = fleurs_langID[i]['audio']['array'].astype(np.float32)
-    audio = whisper.pad_or_trim(audio)
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+with torch.no_grad():
+    for i in tqdm(range(fleurs_langID.num_rows)):
+        audio = fleurs_langID[i]['audio']['array'].astype(np.float32)
+        audio = whisper.pad_or_trim(audio)
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    _, probs = model.detect_language(mel)
-    pred_id = max(probs, key=probs.get)
-    preds.append(get_iso_693_3(pred_id))
-    true_label_id = fleurs_langID[i]['lang_id']
-    true_label.append(get_iso_693_3(decode[true_label_id]))
+        _, probs = model.detect_language(mel)
+        pred_id = max(probs, key=probs.get)
+        pred.append(get_iso_693_3(pred_id))
 
-import pickle as pkl
+        true_label_id = fleurs_langID[i]['lang_id']
+        true.append(decode[true_label_id])
 
-pkl.dump(preds, open(os.path.join(OUTPUT_DIR, "whisper_prediction.pkl"), "wb"))
-pkl.dump(true_label, open(os.path.join(OUTPUT_DIR, "whisper_true_labels.pkl"), "wb"))
-
-from sklearn.metrics import classification_report
-
-class_names = [get_iso_693_3(decode[s]) for s in range(102)]
-print(classification_report(true_label, preds, zero_division=0), file=open(os.path.join(OUTPUT_DIR, 'whisper_report.txt'), "w"))
+pkl.dump(pred, open(os.path.join(OUTPUT_DIR, "whisper_pred.pkl"), "wb"))
+pkl.dump(true, open(os.path.join(OUTPUT_DIR, "whisper_true.pkl"), "wb"))
